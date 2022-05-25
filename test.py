ebe73e00fc151d3ab15c2f3d542b4176ee790ad4
haileadugna <hordofahaile7@gmail.com>
2022-05-25 17:45:47 +0300

    medium

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-lis=[2,4,5,6,7]
-new=list(map(str,lis))
-print(new)
+nums=[3,30,34,5,9]
+temp=[]
+ans=""
+for i in nums:
+    temp.append(str(i))
+temp.sort()
+temp.reverse()
+for k in range(len(temp)):
+    for j in range(len(temp)-1):
+        start = temp[j] + temp[j+1]
+        end = temp[j+1]+temp[j]
+        if end>start:
+            temp[j],temp[j+1]=temp[j+1],temp[j]
+        else:
+            ans += temp[j]
+print(ans)
