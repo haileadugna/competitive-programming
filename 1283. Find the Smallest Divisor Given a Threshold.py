71c563d24e60ca82b2fec1b4a3f8ecdd0f169ccc
haileadugna <hordofahaile7@gmail.com>
2022-08-30 22:29:29 +0300

    medium

---
+++ 1283. Find the Smallest Divisor Given a Threshold.py
@@ -0,0 +1,15 @@
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        l = 1
+        r = max(nums)
+        while l <= r:
+            mid = (r+l)//2
+            summ = sum(math.ceil(i/mid) for i in nums)
+            if summ > threshold:
+                l = mid + 1
+            else:
+                r = mid -1
+        return l 
+                        
+            
+        
