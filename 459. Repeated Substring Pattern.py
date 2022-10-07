740ebfbfbf6254315f3862e84fa146fc03e1a3c6
haileadugna <hordofahaile7@gmail.com>
2022-10-07 13:16:25 +0300

    easy

---
+++ 459. Repeated Substring Pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        # s = list(s)
+        tmp = ""
+        for i in range(len(s)//2):
+            tmp += s[i]
+            if len(s)% len(tmp) == 0:
+                if tmp * (len(s)// len(tmp)) == s:
+                    return True
+        return False
+            
+        
+
+
+
+
+
