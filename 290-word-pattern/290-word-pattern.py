31289695c9cf3176298a1a2c1fc1d31eb307d214
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-01-01 11:20:31 +0300

    Time: 28 ms (94.22%), Space: 13.9 MB (74.33%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -2,6 +2,7 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         dictt = {}
         s = s.split()
+        temp = set()
         if len(s) != len(pattern):
             return False
         for i in range(len(pattern)):
@@ -10,10 +11,11 @@ class Solution:
                     return False
 
             else:
-                if s[i] not in dictt.values():
-                    dictt[pattern[i]] = s[i]
-                else:
+                if s[i] in temp:
                     return False
+                else:
+                    dictt[pattern[i]] = s[i]
+                    temp.add(s[i])
 
         return True
                     
