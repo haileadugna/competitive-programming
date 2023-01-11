36d85e6d3adcbdc04b26e3834922952415bc9526
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-01-11 19:43:43 +0300

    Time: 220 ms (59.34%), Space: 18.4 MB (83.45%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,10 +3,12 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        start =0
-        end = len(s)-1
-        while start <= end:
-            s[start], s[end] = s[end], s[start]
-            start += 1
-            end -= 1
+        
+        s.reverse()
+        # start =0
+        # end = len(s)-1
+        # while start <= end:
+        #     s[start], s[end] = s[end], s[start]
+        #     start += 1
+        #     end -= 1
  
