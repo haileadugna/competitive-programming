f0e156f24c3b64b582a9119843fa7dabf0115a78
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-01-12 18:32:14 +0300

    Time: 52 ms (91.79%), Space: 14.1 MB (95.90%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -5,12 +5,16 @@ class Solution:
             if nums[i] == nums[i+1]:
                 nums[i] = nums[i]*2
                 nums[i+1] = 0
-            
-        res  = []
-        for j in range(len(nums)):
-            if nums[j] != 0:
-                res.append(nums[j])
-                
-        while len(res) < len(nums):
-            res.append(0)
-        return res
+
+        write = 0
+        read = 0
+        while read < len(nums):
+            if nums[read] != 0:
+                temp = nums[read]
+                nums[read] = nums[write]
+                nums[write] = temp
+
+                write =  write + 1
+
+            read = read + 1
+        return nums
