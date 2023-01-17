226220b6902c1ff1280535861e526b264b0784e2
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-01-17 18:55:35 +0300

    Time: 1710 ms (5.03%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,22 +1,26 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
 
-#         for i in range(len(heights)-1,-1,-1):
-#             for j in range(len(heights)-2-i,-1,-1):
-#                 if heights[j] < heights[j+1]:
-#                     heights[j], heights[j+1] = heights[j+1] , heights[j]
-#                     names[j], names[j+1] = names[j+1], names[j]
-
-#         return names
-
         for i in range(len(heights)):
-            ind = i
-            pev = heights[i]
-            for j in range(i, len(heights)):
-                if heights[j] < pev:
-                    ind = j
-                    pev = heights[j]
-            heights[ind], heights[i] = heights[i], heights[ind]
-            names[ind], names[i] = names[i], names[ind]
-        names.reverse()
+            flag = True
+            for j in range(len(heights)-1-i):
+                if heights[j] < heights[j+1]:
+                    heights[j], heights[j+1] = heights[j+1] , heights[j]
+                    names[j], names[j+1] = names[j+1], names[j]
+                    flag = False
+            if flag :
+                break
+
         return names
+
+        # for i in range(len(heights)):
+        #     ind = i
+        #     pev = heights[i]
+        #     for j in range(i, len(heights)):
+        #         if heights[j] < pev:
+        #             ind = j
+        #             pev = heights[j]
+        #     heights[ind], heights[i] = heights[i], heights[ind]
+        #     names[ind], names[i] = names[i], names[ind]
+        # names.reverse()
+        # return names
