48058ec0757a72d85a0429d4bb9205fb2b92f92a
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-02-06 00:54:07 +0300

    Time: 116 ms (79.93%), Space: 15.2 MB (74.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,24 +1,24 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # convert strings s and p to lists
-        s = list(s)
-        p = list(p)
-        # create a Counter object for string p
-        tmp = Counter(p)
-        # set initial values for pointers l and r
-        l, r= 0, len(p)
+        # initialize two lists to store the frequency of each letter in string p and the current window of string s
+        p_count = [0] * 26
+        s_count = [0] * 26
+        # calculate the frequency of each letter in string p
+        for char in p:
+            p_count[ord(char) - ord('a')] += 1
         # initialize an empty list for storing the starting indices of anagrams
         ans = []
-        # loop through the string s, until r pointer goes past the end
-        while r <= len(s):
-            # create a Counter object for the current substring s[l:r]
-            temp = Counter(s[l:r])
-            # check if the Counter objects of p and s[l:r] match
-            if tmp == temp:
-                # if they match, append the starting index l to the list of answers
-                ans.append(l)
-            # move both pointers to the right by one character
-            l += 1
-            r += 1
+        # loop through string s
+        for i in range(len(s)):
+            # increase the frequency of the current letter in the current window
+            s_count[ord(s[i]) - ord('a')] += 1
+            # if the current window size is greater than the size of string p
+            if i >= len(p):
+                # decrease the frequency of the letter that just left the current window
+                s_count[ord(s[i - len(p)]) - ord('a')] -= 1
+            # if the frequency of letters in the current window matches the frequency of letters in string p
+            if s_count == p_count:
+                # append the starting index of the current window to the list of answers
+                ans.append(i - len(p) + 1)
         # return the list of starting indices of anagrams
         return ans
