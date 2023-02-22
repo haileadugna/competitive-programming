05a6a039d4d23aaf78bbb299592abee43a075d1a
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-02-22 14:19:43 +0300

    Time: 136 ms (78.06%), Space: 14.4 MB (54.78%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -0,0 +1,25 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.size = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if carType == 1:
+            if self.size[0] > 0:
+                self.size[0] -= 1
+                return True
+        if carType == 2:
+            if self.size[1] > 0:
+                self.size[1] -= 1
+                return True
+        if carType == 3:
+            if self.size[2] > 0:
+                self.size[2] -= 1
+                return True
+        return False
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
