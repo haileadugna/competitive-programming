7ec90ed4e59711039c78437a2c5007a91a79fb17
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-02-23 17:45:42 +0300

    Time: 1373 ms (22.55%), Space: 14 MB (59.94%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        temp = Counter(s1)
+        s, e = 0, len(s1)
+        while e <= len(s2):
+            ans = Counter(s2[s:e])
+            # print(ans)
+            if ans == temp:
+                return True
+            else:
+                s += 1
+                e += 1
+        return False
