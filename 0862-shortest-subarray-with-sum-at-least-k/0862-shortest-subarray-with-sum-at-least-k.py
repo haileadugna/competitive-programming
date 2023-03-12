85bd4e0f29203e8f603ce9ccdff1c5fc469800a0
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-03-12 20:28:48 +0300

    Time: 1383 ms (39.96%), Space: 28.4 MB (40.72%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -11,23 +11,17 @@ class Solution:
             if nums[i] < 0:
                 nums[i] = 0
         nums = nums[1:]
-        print(len(nums))
+    
         for i in range(len(nums)):
             if nums[i] >= k:
                 while stack and nums[i] - nums[stack[0]] >= k:
-                    l = 1 + stack.popleft()
-                     
-                res = min(res, i - l + 1 )
-            # if nums[i] < 0:
-            #     if i < len(nums):
-            #         l = i
-            #         nums[i + 1] += abs(nums[i])
-            #     else:
-            #         break
-            # print(nums)    
+                    l = 1 + stack.popleft()  
+                res = min(res, i - l + 1 ) 
+                
             while stack and nums[stack[-1]] > nums[i] and nums[i] > 0:
                 stack.pop()
             stack.append(i)
+            
         if res >= len(nums) + 1:
             return -1
         
