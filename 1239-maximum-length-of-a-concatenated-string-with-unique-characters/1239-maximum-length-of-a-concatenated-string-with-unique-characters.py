2aaa2799a16455d5fe6e9365d7e3929a74686098
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-03-21 19:30:47 +0300

    Time: 1447 ms (6.01%), Space: 13.9 MB (83.82%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -2,15 +2,15 @@ class Solution:
     def maxLength(self, arr: List[str]) -> int:
         
         res = 0
-        def backtrack(arr, path):
+        def backtrack(i, arr, path):
             nonlocal res
             if len("".join(path)) == len(set("".join(path))):
                 res = max(res, len("".join(path)))
 
-            for i in range(len(arr)):
-                path.append(arr[i])
-                backtrack(arr[i + 1:], path)
+            for j in range(i+1, len(arr)):
+                path.append(arr[j])
+                backtrack(j,arr, path)
                 path.pop()
           
-        backtrack(arr, [""])
+        backtrack(-1, arr, [""])
         return res
