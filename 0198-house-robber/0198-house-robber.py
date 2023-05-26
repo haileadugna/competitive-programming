3bcbc101921934bac448b20f5c7d2772332aa88f
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-05-26 19:27:21 +0300

    Time: 46 ms (31.91%), Space: 16.3 MB (13.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        n = len(nums) -1
-        @cache
-        def dp(n):
+        temp1 = 0
+        temp2 = 0
+        for i in range(len(nums)):
+            temp = max(nums[i] + temp1, temp2)
+            temp1 = temp2
+            temp2 = temp
+        return temp2
+#         n = len(nums) -1
+#         @cache
+#         def dp(n):
             
-            if n <= 1:
-                return nums[n]
+#             if n <= 1:
+#                 return nums[n]
 
-            if n ==  2:
-                return nums[n] + nums[0]
+#             if n ==  2:
+#                 return nums[n] + nums[0]
 
-            return nums[n] + max(dp(n-2), dp(n - 3))
+#             return nums[n] + max(dp(n-2), dp(n - 3))
             
-        return max(dp(n), dp(n -1))
+#         return max(dp(n), dp(n -1))
