fd719c7ac6a1b8078853810ee28d4874fcad3ec2
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-06-08 21:30:37 +0300

    Time: 87 ms (43.40%), Space: 18 MB (7.37%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,9 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        # nums.sort()
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j]== target:
-                    return [i, j]
+            nums[i] = [nums[i], i]
         
+        nums.sort()
+        
+        start = 0
+        end = len(nums) -1
+        
+        while start < end:
+            
+            if nums[end][0] + nums[start][0] > target:
+                end -= 1
+                
+            elif nums[end][0] + nums[start][0] < target:
+                start += 1
+                
+                
+            else:
+                return [nums[start][-1], nums[end][-1]]
