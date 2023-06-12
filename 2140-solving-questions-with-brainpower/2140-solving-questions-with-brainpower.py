a9aa510cfeaaaca5823b61b171564f004ac27216
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-06-12 23:57:37 +0300

    Time: 1863 ms (5.06%), Space: 192.5 MB (9.26%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        
+        @cache
+        def dfs(i):
+            if i >= len(questions):
+                return 0
+            
+            
+            return max(dfs(i+1), questions[i][0] + dfs(i + 1 + questions[i][1]))
+        
+        return dfs(0)
