593ed04e80c485c429709be259d489fd439d78ac
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-10-18 23:07:33 +0300

    Time: 94 ms (91.09%), Space: 18.5 MB (98.62%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,11 +2,16 @@ class Solution:
     def calculate(self, s: str) -> int:
         
         i, j, res, sign = 0, 0, 0, '+'
+        
         for c in s + '+':
-            if c == ' ': continue
+            
+            if c == ' ':
+                continue
+                
             if c.isdigit():
                 i = 10 * i + int(c)
                 continue
+                
             if sign == '+':
                 res += j
                 j = i
@@ -20,6 +25,7 @@ class Solution:
                 
             elif sign == '/':
                 j = int(j / i)
+                
             i, sign = 0, c
             
         return res + j
