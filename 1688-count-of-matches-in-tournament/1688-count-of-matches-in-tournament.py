a2d3d67d51893efb87a0f62be3d5f6e703aefe7b
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-12-05 21:49:01 +0300

    Time: 41 ms (32.17%), Space: 16.2 MB (71.13%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        
+        res = 0
+        while n > 1:
+            if n%2 == 0:
+                n //= 2
+                res += n
+                
+            else:
+                n = (n -1)//2 + 1
+                res += n -1
+                
+        return res
+            
